35b808dcbabf1a043039fa583fb9fcbe6684ce6e
Preston Jennings <prestonj@gmail.com>
2015-05-06 17:27:54 -0400

    Picked up brainCloud change and added Identity screen to auth demo to test logout feature.

---
+++ Authentication/Assets/Scripts/ScreenIdentity.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using BrainCloud;
+using BrainCloud.Common;
+using LitJson;
+
+public class ScreenIdentity : BCScreen {
+	
+	public override void Activate()
+	{
+	}
+
+	public override void OnScreenGUI()
+	{
+		GUILayout.BeginVertical();
+
+		GUILayout.BeginHorizontal();
+		if (GUILayout.Button("Logout"))
+		{
+			BrainCloudWrapper.GetBC().PlayerStateService.Logout(Logout_Success, Failure_Callback, null);
+		}
+		GUILayout.EndHorizontal();
+		
+		GUILayout.EndVertical();
+    }
+
+	private void Logout_Success(string json, object o)
+	{
+		Application.LoadLevel("Connect");
+	}
+}
