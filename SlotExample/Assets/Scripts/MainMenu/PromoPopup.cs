e9fae49fc27b9111354fd37731bf4b51dd21102c
Brad Hill <bradleyh@bitheads.com>
2015-08-18 14:15:20 -0400

    Started work on promo popup

---
+++ SlotExample/Assets/Scripts/MainMenu/PromoPopup.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class PromoPopup : MonoBehaviour
+{
+    public GameObject PopupObject;
+
+    public void OnClickPromoButton()
+    {
+        PopupObject.SetActive(true);
+    }
+
+    public void OnClickPurchase()
+    {
+        PopupObject.SetActive(false);
+    }
+
+    public void OnClickNoThanks()
+    {
+        PopupObject.SetActive(false);
+    }
+}
