0fcf12ee5d8bcb747165bfb89e163602681aaa04
Jonathan McCaffrey <jonmcc.0723@gmail.com>
2018-12-19 13:48:26 -0500

    Fixing committed example ids

--- SpaceShooterWithStats/Assets/Editor/BrainCloudExampleStartup.cs
+++ SpaceShooterWithStats/Assets/Editor/BrainCloudExampleStartup.cs
@@ -8,7 +8,7 @@ public class BrainCloudExampleStartup : MonoBehaviour {
 	static BrainCloudExampleStartup()
 	{
 		// Setting the default template id.
-		PlayerPrefs.SetString(BrainCloudPluginSettings.DEFAULT_TEMPLATE_ID_KEY, "112186");
-		PlayerPrefs.SetString(BrainCloudPluginSettings.DEFAULT_TEMPLATE_NAME_KEY, "TicTacToe");
+		PlayerPrefs.SetString(BrainCloudPluginSettings.DEFAULT_TEMPLATE_ID_KEY, "12186");
+		PlayerPrefs.SetString(BrainCloudPluginSettings.DEFAULT_TEMPLATE_NAME_KEY, "Space Shooter");
 	}
 }
