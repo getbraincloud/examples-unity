c607634a2224c8842baf12a1994821b533d1b1b5
BITHEADSINC\markd <markd@bitheads.com>
2019-07-22 16:10:39 -0400

    Commiting Pierre's changes

--- TicTacToe/Assets/Scripts/Menus/GameScene.cs
+++ TicTacToe/Assets/Scripts/Menus/GameScene.cs
@@ -1,17 +1,29 @@
 ﻿#region
-
+using TMPro;
 using UnityEngine;
-
+using UnityEngine.UI;
 #endregion
 
 public class GameScene : MonoBehaviour
 {
     public App App;
-    
+
+    [SerializeField] public TMP_InputField UserName;
+    [SerializeField] public TextMeshProUGUI SkillRating;
+    [SerializeField] public Image UserNameBG;
+
+    private void Update()
+    {
+        if (SkillRating != null)
+            SkillRating.text = "Skill Rating: " + App.PlayerRating;
+    }
+
     public void OnEditName()
     {
         editablePlayerName = App.Name;
         isEditingPlayerName = true;
+        UserName.interactable = true;
+        UserNameBG.gameObject.SetActive(true);
     }
 
     public void OnGoToLeaderboardScene()
@@ -29,6 +41,24 @@ public class GameScene : MonoBehaviour
         App.GotoMatchSelectScene(gameObject);
     }
 
+    public void OnEndEditName(string str)
+    {
+        UserName.text = UserName.text.Trim();
+        App.Name = UserName.text;
+        isEditingPlayerName = false;
+
+        App.Bc.PlayerStateService.UpdateUserName(App.Name,
+            (response, cbObject) => { },
+            (status, code, error, cbObject) => { Debug.Log("Failed to change Player Name"); });
+    }
+
+    public void OnLogout()
+    {
+        App.Bc.PlayerStateService.Logout((response, cbObject) => { App.GotoLoginScene(gameObject); });
+        PlayerPrefs.SetString(App.WrapperName + "_hasAuthenticated", "false");
+    }
+
+    // TODO: Get rid of this when the new Achievements and Leaderboard screens are updated.
     protected void OnPlayerInfoWindow(int windowId)
     {
         GUILayout.BeginHorizontal();
@@ -62,22 +92,22 @@ public class GameScene : MonoBehaviour
         GUILayout.EndHorizontal();
 
         GUILayout.Label(string.Format("PlayerRating: {0}", App.PlayerRating), GUILayout.MinWidth(200));
-        
+
         GUILayout.FlexibleSpace();
-        
+
         GUILayout.BeginVertical();
-        
-        if(GetType() != typeof(Leaderboard))
+
+        if (GetType() != typeof(Leaderboard))
             if (GUILayout.Button("Leaderboard", GUILayout.MinWidth(50))) OnGoToLeaderboardScene();
 
-        if(GetType() != typeof(Achievements))
+        if (GetType() != typeof(Achievements))
             if (GUILayout.Button("Achievements", GUILayout.MinWidth(50))) OnGoToAchievementsScene();
-        
-        if(GetType() != typeof(MatchSelect))
+
+        if (GetType() != typeof(MatchSelect))
             if (GUILayout.Button("MatchSelect", GUILayout.MinWidth(50))) OnGoToMatchSelectScene();
 
         GUILayout.FlexibleSpace();
-        
+
         GUILayout.EndVertical();
 
         GUILayout.EndVertical();
