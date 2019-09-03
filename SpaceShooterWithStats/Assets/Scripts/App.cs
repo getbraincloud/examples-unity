5b947e224488958df96f1e99f12a698c0e9ebafb
pascalf1309 <54288832+pascalf1309@users.noreply.github.com>
2019-09-03 12:41:07 -0400

    CleanedUp (#65)
    * CleanedUp
    
    * Formating

--- SpaceShooterWithStats/Assets/Scripts/App.cs
+++ SpaceShooterWithStats/Assets/Scripts/App.cs
@@ -1,4 +1,7 @@
 ﻿using UnityEngine;
+using BrainCloud;
+using System.Collections.Generic;
+using BrainCloudUnity;
 
 public class App : MonoBehaviour
 {    
@@ -12,8 +15,15 @@ public class App : MonoBehaviour
         DontDestroyOnLoad(this); // on an Object that won't be destroyed on Scene Changes
 
         Bc.WrapperName = WrapperName; // Optional: Add a WrapperName
-        Bc.Init(); // Required: Initialize the Wrapper   
-        
+
+        Dictionary<string, string> apps = new Dictionary<string, string>();
+        apps[BrainCloudSettingsManual.Instance.GameId] = BrainCloudSettingsManual.Instance.SecretKey;
+        Bc.InitWithApps(BrainCloudSettingsManual.Instance.DispatcherURL, BrainCloudSettingsManual.Instance.GameId, apps, BrainCloudSettingsManual.Instance.GameVersion);
+
+        //If you want to add additional apps, for parents/child, add it here, the client handles switching correctly
+
         Bc.Client.EnableLogging(true);
     }
 }
+
+//Facebook.dll errors upon opening unity is a known on going bug in Unity but does not interfere with this code base. You can follow information regarding this bug here https://issuetracker.unity3d.com/issues/facebook-editor-throw-errors-related-to-sdk-after-facebook-support-installation
