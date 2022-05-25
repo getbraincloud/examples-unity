1fb70a20cf441f4a61e2d214f71eb5fbc607b843
anthonyd94 <anthonyd@bitheads.com>
2022-05-25 12:07:06 -0400

    Cloud Code Page
    +Added logic to allow cloud code params to be passed via input field.
    +Logic to interact with cloud code scripts

--- Authentication/Assets/Scripts/ScreenCloudCode.cs
+++ Authentication/Assets/Scripts/ScreenCloudCode.cs
@@ -4,17 +4,83 @@ using BrainCloud;
 using BrainCloud.Common;
 using System;
 using BrainCloud.LitJson;
+using UnityEngine.UI; 
 
 public class ScreenCloudCode : BCScreen
 {
+    [SerializeField] Text cloudScriptText;
+    [SerializeField] Text placeholderJsonExample; 
+
     private string m_cloudCodeScript = "";
     private string m_cloudCodeData = "";
+    private string m_exampleJSONPlaceholder = ""; 
+
+    private static string[] m_cloudScriptNames;
 
     public ScreenCloudCode(BrainCloudWrapper bc) : base(bc) { }
-    
+
+    private void Awake()
+    {
+        m_cloudScriptNames = new string[] { "HelloWorld", "IncrementGlobalStat", "IncrementPlayerStat" };
+        OnTemplateChanged(0);
+    }
+
     public override void Activate(BrainCloudWrapper bc)
     {
-        _bc = bc; 
+        _bc = bc;
+    }
+
+    public void OnTemplateChanged(int index)
+    {
+        m_cloudCodeScript = m_cloudScriptNames[index];
+        cloudScriptText.text = m_cloudCodeScript;
+
+        switch(m_cloudCodeScript)
+        {
+            case "HelloWorld":
+                placeholderJsonExample.text = "Example Parameters...\n{\"name\" : \"your_name\", \n\"age\" : your_age}";
+                break;
+            case "IncrementGlobalStat":
+                placeholderJsonExample.text = "Example Parameters...\n{\"globalStat\" : \"PLAYER_COUNT\", \n\"incrementAmount\" : 1}";
+                break;
+            case "IncrementPlayerStat":
+                placeholderJsonExample.text = "Example Parameters...\n{\"playerStat\" : \"experiencePoints\", \n\"incrementAmount\" : 1}";
+                break;
+        }
+    }
+
+    public void OnParamEndEdit(string param)
+    {
+        m_cloudCodeData = param;
+
+        try
+        {
+            if (m_cloudCodeData.Length > 0)
+            {
+                JsonMapper.ToObject(m_cloudCodeData);
+            }
+        }
+        catch (Exception e)
+        {
+            // log and rethrow
+            Debug.LogError("FAILED TO PARSE JSON: " + e.ToString());
+            throw;
+        }
+    }
+
+    public void OnRunScriptClick()
+    {
+        _bc.ScriptService.RunScript(m_cloudCodeScript, m_cloudCodeData, OnRunScriptSuccess, OnRunScriptFailure);
+    }
+
+    public void OnRunScriptSuccess(string jsonResponse, object cbObject)
+    {
+        //Debug.LogWarning(m_cloudCodeScript + " Script ran successfully");
+    }
+
+    public void OnRunScriptFailure(int status, int reasonCode, string jsonErrorm, object cbObject)
+    {
+        Debug.LogError(m_cloudCodeScript + " Script failed to run.");
     }
 
     public override void OnScreenGUI()
@@ -83,7 +149,6 @@ public class ScreenCloudCode : BCScreen
         }
         GUILayout.EndHorizontal();
         GUILayout.EndVertical();
-
     }
 
     private void CloudCode_Success(string json, object cb)
