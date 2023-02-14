1cbcae8d32d9622e94e1b235abc4d297c5763dcb
Michael Costa <michaelc@bitheads.com>
2023-02-13 20:01:13 -0500

    Reorganize UI, better UI Interactables

--- Authentication/Assets/App/Scripts/UI/ContentUI/LoggerContentUI.cs
+++ Authentication/Assets/App/Scripts/UI/ContentUI/LoggerContentUI.cs
@@ -103,7 +103,7 @@ public class LoggerContentUI : MonoBehaviour, IContentUI
 
     private void OnDestroy()
     {
-        logGOs.Clear();
+        logGOs?.Clear();
         logGOs = null;
     }
 
