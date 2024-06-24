6423bc2304bc1c81ac5bbc6c0f074771412be29f
SpencerH <spencerh@bitheads.com>
2024-06-24 17:18:08 -0400

    Added Paint Assets

---
+++ RelayTestApp/Assets/Scripts/UI/AnimateSplatter.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AnimateSplatter : MonoBehaviour
+{
+    private void OnEnable()
+    {
+        
+    }
+}
